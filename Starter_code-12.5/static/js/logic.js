3cb0ef580509fb00ddc58e8bded5b437c0a5b751
Samrosegithub <samuelrose322@gmail.com>
2023-02-16 22:05:02 -0600

    new_shit

--- Starter_code-12.5/static/js/logic.js
+++ Starter_code-12.5/static/js/logic.js
@@ -1,5 +1,4 @@
-const url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_week.geojson";
-
+const url = "https://data.nasa.gov/api/views/gh4g-9sfh/rows.json?accessType=DOWNLOAD";
 /* // Promise Pending
 const dataPromise = d3.json(url);
 console.log("Data Promise: ", dataPromise);
@@ -8,7 +7,6 @@ console.log("Data Promise: ", dataPromise);
 // Creating our initial map object:
 // We set the longitude, latitude, and starting zoom level.
 // This gets inserted into the div with an id of "map".
-
 var myMap = L.map("map", {
     center: [45.52, -122.67],
     zoom: 3
@@ -21,7 +19,7 @@ var myMap = L.map("map", {
 }).addTo(myMap);
 // Creating a new marker:
 // We pass in some initial options, and then add the marker to the map by using the addTo() method.\
-d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
+d3.json("https://data.nasa.gov/api/views/gh4g-9sfh/rows.json?accessType=DOWNLOAD").then(function(data) {
   console.log(data);
   function getColor(depth) {
     switch (true) {
@@ -37,11 +35,10 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
         return "#d4ee00";
       case depth > -10:
         return "#98ee00";
-    }
+     }
+  
   }
-
 colors = ["#800026", "#BD0026", "#E31A1C", "#FC4E2A", "#FD8D3C", "#FED976"]
-
 limits = [90, 70, 50, 30, 10, -10]
 
 function style(feature) {
@@ -66,7 +63,6 @@ function style(feature) {
         + "<br>Depth: "
         + feature.geometry.coordinates[2]
         + "<br>Location: "
-
       );
     }
   }).addTo(myMap);
@@ -77,7 +73,7 @@ legend.onAdd = function (myMap) {
 
     var div = L.DomUtil.create('div', 'info legend'),
         grades = [90, 70, 50, 30, 10, -10],
-        lcolors = ["#800026", "#BD0026", "#E31A1C", "#FC4E2A", "#FD8D3C", "#FED976"]
+        colors = ["#800026", "#BD0026", "#E31A1C", "#FC4E2A", "#FD8D3C", "#FED976"]
 
     // loop through our density intervals and generate a label with a colored square for each interval
     for (var i = 0; i < grades.length; i++) {
@@ -93,19 +89,19 @@ legend.onAdd = function (myMap) {
   }
 
 legend.addTo(myMap);
-  });
+});
 
-
+ 
 /* 
 const url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_week.geojson";
 
-/* // Promise Pending
+Promise Pending
 const dataPromise = d3.json(url);
 console.log("Data Promise: ", dataPromise);
 
 // Fetch the JSON data and console log it
  */
-
+ 
 // Creating our initial map object:
 // We set the longitude, latitude, and starting zoom level.
 // This gets inserted into the div with an id of "map".
